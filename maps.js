5d3ffa83e1aceab78fbed9fa38051c9ac8e557e7
Jakob Antonsson <jakobant@tempo.io>
2017-12-17 15:58:42 +0000

    refactor mapjs

--- maps.js
+++ maps.js
@@ -1,3 +1,4 @@
+//delete not used
 $(document).ready(function () {
         $(".preview").hover(function(){
             $(this).find('img').fadeIn();
@@ -26,22 +27,23 @@ FixedQueue.trimHead = function(){
     Array.prototype.splice.call( this, 0, (this.length - this.fixedSize) );
 };
 FixedQueue.trimTail = function(){
-  if (this.length <= this.fixedSize) { return; }
-  Array.prototype.splice.call( this, this.fixedSize, (this.length - this.fixedSize)
-  );
+    if (this.length <= this.fixedSize) { return; }
+    Array.prototype.splice.call( this, this.fixedSize, (this.length - this.fixedSize)
+    );
 };
 FixedQueue.wrapMethod = function( methodName, trimMethod ){
-  var wrapper = function(){
+    var wrapper = function(){
     var method = Array.prototype[ methodName ];
     var result = method.apply( this, arguments );
     trimMethod.call( this );
     return( result );
-  };
-  return( wrapper );
+    };
+    return( wrapper );
 };
 FixedQueue.push = FixedQueue.wrapMethod( "push", FixedQueue.trimHead );
 FixedQueue.splice = FixedQueue.wrapMethod( "splice", FixedQueue.trimTail );
 FixedQueue.unshift = FixedQueue.wrapMethod( "unshift", FixedQueue.trimTail );
+
 var airports = [];
 d3.csv("airports.csv", function(data) { airports = data;  });
 d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vS_YSj972k5Do_gJJ-j_Rxx5-f7gU8ofh1jGNcQyTWiYxioQtDBtuZQyIYSGqEh2ZVuCBpumqL1K3CC/pub?output=csv", function(data) { flightdata = data; });
@@ -143,39 +145,9 @@ function appendFlightRow(id, flight, num) {
     }
     element.appendChild(tr);
 }
-function appendFlightRowSimple(id, flight) {
-    //date
-    var trstart = '<td>'+ flight.date +'</td>';
-    //flag
-    var imgname = flight.country.replace('United Arab Emirates', 'UEA').replace(' ', '_');
-    var path = 'https://www.free-country-flags.com/countries/' + imgname + '/1/tiny/' + imgname + '.png';
-    var trflag = '<td><img src="'+ path +'"></td>';
-    // to country
-    var tocountry = '<td>'+ flight.country +'</td>';
-    // to city
-    var tocity = '<td>'+ flight.city +'</td>';
-    // competition
-    var link_text = '<a href="#" class="preview">'+ flight.competition +'<img src="'+ flight.img +'" class="hide-image" style="z-index: 100; position: absolute;"/>';
-    var compet = '<td>'+ link_text +'</td>';
 
-    var element = document.getElementById(id);
-    var rowCount = element.rows.length;
-    // Only allow 50 rows
-    if (rowCount >= 25) {
-        element.removeChild(element.childNodes[0]);
-    }
-    element.app
-    tr.appendChild(trstart+trflag+tocountry+tocity+compet);
-    element.appendChild(tr);
-}
 function handleFlights(msg, num) {
-    var flightList = [
-              msg.date,
-              msg.competition,
-              getAirport(flightdata[f].to).country,
-              getAirport(flightdata[f].to).country];
     appendFlightRow('flight-tracking', msg, num);
-    //prependFlightRow('flight-tracking', flightList);
 }
 
 var hits = FixedQueue( 20, [  ] );
@@ -207,7 +179,7 @@ var f = 0;
 var randAir = getRandomInt(1, 10);
 var to_airport = {};
 var flights = {
-    interval: 2000,
+    interval: 3000,
     init: function(){
        setTimeout(
            jQuery.proxy(this.getData, this),
@@ -216,32 +188,45 @@ var flights = {
     },
     getData: function() {
     var self = this;
-        //if (flightdata[f].home == 'true') { strokeColor = 'yellow'; strokeWidth = 1; arcSharpness = 0.8; }
-        if (flightdata[f].type == 'flight') { strokeColor = 'green'; strokeWidth = 2; arcSharpness = 1.5; }
-        else if (flightdata[f].type == 'train') { strokeColor = 'blue'; strokeWidth = 2; arcSharpness = 1.3; }
-        else if (flightdata[f].type == 'car') { strokeColor = 'red'; strokeWidth = 2; arcSharpness = 1.1; }
-        else { strokeColor = 'pink'; strokeWidth = 1; arcSharpness = 0.2; }
-        if (flightdata[f].home) { strokeColor = 'yellow'; strokeWidth = 1; arcSharpness = 0.8; }
-        if (flightdata[f].from  == flightdata[f].to) { strokeColor = 'yellow'; strokeWidth = 0; arcSharpness = 0; }
+        if (flightdata[f].type == 'flight') {
+            strokeColor = 'green'; strokeWidth = 2; arcSharpness = 1.5; }
+        else if (flightdata[f].type == 'train') {
+            strokeColor = 'blue'; strokeWidth = 2; arcSharpness = 1.3; }
+        else if (flightdata[f].type == 'car') {
+            strokeColor = 'red'; strokeWidth = 2; arcSharpness = 1.1; }
+        else {
+            strokeColor = 'pink'; strokeWidth = 1; arcSharpness = 0.2; }
+        if (flightdata[f].home) {
+            strokeColor = 'yellow'; strokeWidth = 1; arcSharpness = 0.8; }
+        if (flightdata[f].from  == flightdata[f].to) {
+            strokeColor = 'yellow'; strokeWidth = 0; arcSharpness = 0; }
+        if (flightdata[f].home == 'true') {
+        strokeWidth = 0.7; arcSharpness = 0.8; }
+
         handleFlights(flightdata[f], f);
-        if (flightdata[f].home == 'true') { strokeWidth = 0.7; arcSharpness = 0.8; }
+
         hits.push( { origin : { latitude: getAirport(flightdata[f].from).latitude, longitude: getAirport(flightdata[f].from).longitude },
             destination : { latitude: getAirport(flightdata[f].to).latitude, longitude: getAirport(flightdata[f].to).longitude } } );
-        map.arc(hits, {strokeWidth: strokeWidth, strokeColor: strokeColor, arcSharpness: arcSharpness, animationSpeed: 1200});
+        map.arc(hits, {strokeWidth: strokeWidth, strokeColor: strokeColor, arcSharpness: arcSharpness, animationSpeed: 2200});
         var res = '';
-        if (flightdata[f].success) { res = 'Rank: '+ flightdata[f].success; }
-        if (flightdata[f].video_id) { var media_link = '<a href="http://www.youtube.com/watch?feature=player_embedded&v=' + flightdata[f].video_id + '" target="_blank"><img src="http://img.youtube.com/vi/' + flightdata[f].video_id + '/0.jpg" alt="" width="90" height="auto" border="0" /></a>'; }
-        else if (flightdata[f].video) { var media_link = '<a href="' + flightdata[f].video + '" target="_blank"><img src="' + flightdata[f].img + '" alt="" width="90" height="auto" border="0" /></a>'; }
-        else if (flightdata[f].img) { var media_link = '<img src="' + flightdata[f].img + '" alt="" width="90" height="auto" border="0" />'; }
-        else { var media_link = '<a href="https://www.facebook.com/nicoloandsara/" target="_blank"><img src="https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/25299069_1563821856986532_6757225713160239883_n.jpg?oh=4076e10d808812731f55e73b0ed6394e&oe=5AB86C06" alt="" width="90" height="auto" border="0" /></a>'; }
+        if (flightdata[f].success) {
+            res = 'Rank: '+ flightdata[f].success; }
+        if (flightdata[f].video_id) {
+            var media_link = '<a href="http://www.youtube.com/watch?feature=player_embedded&v=' + flightdata[f].video_id + '" target="_blank"><img src="http://img.youtube.com/vi/' + flightdata[f].video_id + '/0.jpg" alt="" width="90" height="auto" border="0" /></a>'; }
+        else if (flightdata[f].video) {
+            var media_link = '<a href="' + flightdata[f].video + '" target="_blank"><img src="' + flightdata[f].img + '" alt="" width="90" height="auto" border="0" /></a>'; }
+        else if (flightdata[f].img) {
+            var media_link = '<img src="' + flightdata[f].img + '" alt="" width="90" height="auto" border="0" />'; }
+        else {
+            var media_link = '<a href="https://www.facebook.com/nicoloandsara/" target="_blank"><img src="https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/25299069_1563821856986532_6757225713160239883_n.jpg?oh=4076e10d808812731f55e73b0ed6394e&oe=5AB86C06" alt="" width="90" height="auto" border="0" /></a>'; }
         var bub_text = 'Date: '+flightdata[f].date+ '<br>C: ' + flightdata[f].competition+ '<br>Type:' + flightdata[f].dance_type+ '<br>' + res;
+
         boom.push( { radius: 8, latitude: getAirport(flightdata[f].to).latitude, longitude: getAirport(flightdata[f].to).longitude,
             fillOpacity: 1, text: bub_text, img: flightdata[f].img, media_link: media_link} );
+
         map.bubbles(boom, {
             popupTemplate: function(geo, data) {
             return '<div class="hoverpop"><div class="fixed">' + data.text + '</div><div class="flex-item">'+ data.media_link+'</div></div>';
-            //return '<div class="hoverpop"><div class="fixed">' + data.text + '</div><div class="flex-item"><img src="'+ data.img+'" height=75 width=75></div></div>';
-            //<div style="width: 100%; overflow: hidden;">
         } });
 
         f++;
@@ -252,7 +237,6 @@ var flights = {
 };
 
 
-
 $(document).on("click","#informIP #exit", function (e) {
     $("#informIP").hide();
 });
@@ -260,18 +244,30 @@ $(document).on("click","#informIP #exit", function (e) {
 $(document).on("click", '#flight-tracking .showInfo', function(e) {
     console.log($(this));
     var index = $(this).attr("id");
-    //console.log(flightdata[index]);
-    if (flightdata[index].video_id) { var media_link = '<a href="http://www.youtube.com/watch?feature=player_embedded&v=' + flightdata[index].video_id + '" target="_blank"><img src="http://img.youtube.com/vi/' + flightdata[index].video_id + '/0.jpg" alt="" width="320" height="auto" border="0" /></a>'; }
-    else if (flightdata[index].video) { var media_link = '<a href="' + flightdata[index].video + '" target="_blank"><img src="' + flightdata[index].img + '" alt="" width="320" height="auto" border="0" /></a>'; }
-    else if (flightdata[index].img) { var media_link = '<img src="' + flightdata[index].img + '" alt="" width="320" height="auto" border="0" />'; }
-    else { var media_link = '<a href="https://www.facebook.com/nicoloandsara/" target="_blank"><img src="https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/25299069_1563821856986532_6757225713160239883_n.jpg?oh=4076e10d808812731f55e73b0ed6394e&oe=5AB86C06" alt="" width="320" height="auto" border="0" /></a>'; }
+    if (flightdata[index].video_id) {
+        //embedded auto play
+        //var media_link = '<iframe ' + flightdata[index].video_id + '&autoplay=1></iframe>"';
+        var media_link = '<a href="http://www.youtube.com/watch?feature=player_embedded&v=' + flightdata[index].video_id + '" target="_blank"><img src="http://img.youtube.com/vi/' + flightdata[index].video_id + '/0.jpg" alt="" width="320" height="auto" border="0" /></a>';
+    }
+    else if (flightdata[index].video) {
+        var media_link = '<a href="' + flightdata[index].video + '" target="_blank"><img src="' + flightdata[index].img + '" alt="" width="320" height="auto" border="0" /></a>';
+    }
+    else if (flightdata[index].link) {
+        var media_link = '<a href="' + flightdata[index].link + '" target="_blank"><img src="' + flightdata[index].img + '" alt="" width="320" height="auto" border="0" /></a>';
+    }
+    else if (flightdata[index].img) {
+        var media_link = '<img src="' + flightdata[index].img + '" alt="" width="320" height="auto" border="0" />';
+    }
+    else {
+        var media_link = '<a href="https://www.facebook.com/nicoloandsara/" target="_blank"><img src="https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/25299069_1563821856986532_6757225713160239883_n.jpg?oh=4076e10d808812731f55e73b0ed6394e&oe=5AB86C06" alt="" width="320" height="auto" border="0" /></a>';
+    }
     $("#informIP").show();
     $("#informIP").html( '<button id="exit">Close</button><h3>'+ flightdata[index].competition +'</h3>'+
-    '<div class="hoverpop"><div class="fixed"><br>Country: ' + flightdata[index].country + ' ' + flightdata[index].city +
-    '<br>Dance: ' + flightdata[index].dance_type +
-    '<br>Rank: ' + flightdata[index].success + '</div><div class="flex-item">' +
-    media_link );
+            '<div class="hoverpop"><div class="fixed"><br>Country: ' + flightdata[index].country + ' ' + flightdata[index].city +
+            '<br>Dance: ' + flightdata[index].dance_type +
+            '<br>Rank: ' + flightdata[index].success + '</div><div class="flex-item">' +
+            media_link );
 });
 
-// start the ball rolling!
+// start the geo flights flowting!
 flights.init();
